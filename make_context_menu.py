1dd3bb9b90f76e07059f617c0d7b5e69d7928ff4
Atlinx <atlinuxtm@gmail.com>
2022-01-26 01:59:50 -0500

    Add progress indicators

--- make_context_menu.py
+++ make_context_menu.py
@@ -11,15 +11,19 @@ def exec_menu_command(file_names, params):
         output = PdfFileWriter()
         input1 = PdfFileReader(inputstream)
 
-        for page in input1.pages[1:]:
-          og_height = 596
-          target_size = (int(8.5*72), int(11*72))
-          scale_factor = target_size[1] / og_height
-          translation = (-72 * scale_factor, -160 * scale_factor)
+        og_height = 596
+        target_size = (int(8.5*72), int(11*72))
+        scale_factor = target_size[1] / og_height
+        translation = (-72 * scale_factor, -161 * scale_factor)
           
+        idx = 0
+        for page in input1.pages[1:]:
+          idx += 1
+          print(f'Starting new page: {idx}')
           newPage = PageObject.createBlankPage(None, target_size[0], target_size[1])
           newPage.mergeScaledTranslatedPage(page, scale_factor, translation[0], translation[1])
 
+          print(f'Adding new page: {idx}')
           output.addPage(newPage)
 
         # finally, write "output" to document-output.pdf
